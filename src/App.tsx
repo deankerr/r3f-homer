47380ac55a174d18e14e78a937d623629b3b6273
Dean Kerr <dean.kerr@gmail.com>
2023-06-19 04:19:11 +1000

    Model experiments

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,21 @@ import {
   TorusKnot,
 } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { GridHelper, TorusGeometry } from 'three'
 
+import { AlarmClock } from './AlarmClock'
 import Floor from './Floor'
-import { Gnome } from './Gnome'
-import Lights from './Lights'
+import { Bust } from './Model/Bust'
+import { Chair } from './Model/Chair'
+import GFClock from './Model/GFClock'
+import { Gnome } from './Model/Gnome'
+import { Gnome2 } from './Model/GnomeT'
+import { Grandfather } from './Model/Grandfather'
+import { Lime } from './Model/Lime'
+import Skull from './Model/Skull'
 import Box from './Shape/Box'
 import Cylinder from './Shape/Cylinder'
 import Flower from './Shape/Flower'
 import Sphere from './Shape/Sphere'
-import Skull from './Skull'
 
 function App() {
   return (
@@ -44,7 +49,13 @@ function App() {
           </Float>
           <Cylinder position={[0, 6, 0]} />
           <Flower position-y={10} />
+
           <Gnome scale={10} />
+          <Gnome2 scale={10} position={[-2, 0, 0]} />
+          <Bust scale={10} />
+          <Chair scale={10} position={[-5, 0, 0]} />
+          <Lime scale={10} position={[5, 0, 0]} />
+
           {/* <Floor /> */}
           <axesHelper args={[10]} />
           <OrbitControls target={[0, 0, 0]} />
@@ -66,7 +77,7 @@ function App() {
           </Text3D>
         </Center>
 
-        <Cloud position={[0, -10, 0]} />
+        {/* <Cloud position={[0, -10, 0]} /> */}
         <TorusKnot args={[100, 1]} material-color="hotpink" />
 
         <Stats />
