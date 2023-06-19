0d9e3ef174752c36d34a9f15c2745813a542cfd3
Dean Kerr <dean.kerr@gmail.com>
2023-06-19 23:22:18 +1000

    Grow Homers arms slowly

--- src/Homer/Arm.tsx
+++ src/Homer/Arm.tsx
@@ -1,8 +1,19 @@
 import { Capsule } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Mesh } from 'three'
+
+const armGrowthSpeed = 0.00005
 
 type Props = JSX.IntrinsicElements['group'] & { skinColor: string }
 
 export function Arm({ skinColor, ...group }: Props) {
+  // grow arms
+  const armRef = useRef<Mesh>(null!)
+  useFrame(() => {
+    armRef.current.scale.setY(armRef.current.scale.y + armGrowthSpeed)
+  })
+
   return (
     <group {...group}>
       {/* sleeves */}
@@ -12,7 +23,8 @@ export function Arm({ skinColor, ...group }: Props) {
 
       {/* arm meat */}
       <Capsule
-        args={[0.5, 3.5]}
+        ref={armRef}
+        args={[0.5, 2.5]}
         rotation={[Math.PI / 2, 0, 0]}
         position={[0, 0, 1.75]}
       >
