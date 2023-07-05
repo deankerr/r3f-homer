950cee47a24812e98433cff446d83baf6b40a0f9
Dean Kerr <dean.kerr@gmail.com>
2023-07-06 02:37:15 +1000

    Adjust URLText pos/rot

--- src/scenes/Pyramid/components/URLText.tsx
+++ src/scenes/Pyramid/components/URLText.tsx
@@ -12,10 +12,10 @@ export function URLText({ text }: Props) {
     'URL 3D Text',
     {
       x: { value: 80, min: -500, max: 500, step: 1 },
-      y: { value: 80, min: 0, max: 200, step: 1 },
+      y: { value: 50, min: 0, max: 200, step: 1 },
       z: { value: -120, min: -500, max: 500, step: 1 },
-      rotationX: { value: 0.7, min: -Math.PI, max: Math.PI, step: 0.1 },
-      rotationY: { value: -0.6, min: -Math.PI, max: Math.PI, step: 0.1 },
+      rotationX: { value: 0.4, min: -Math.PI, max: Math.PI, step: 0.1 },
+      rotationY: { value: -0.7, min: -Math.PI, max: Math.PI, step: 0.1 },
       rotationZ: { value: 0.3, min: -Math.PI, max: Math.PI, step: 0.1 },
     },
     { collapsed: true }
@@ -24,14 +24,18 @@ export function URLText({ text }: Props) {
   const mainColor = usePyramidStore((state) => state.mainColor)
 
   return (
-    <Float>
-      <Center position={[x, y, z]} rotation={[rotationX, rotationY, rotationZ]}>
-        <Text3D font={'font/bigblue.json'} size={6} height={10}>
-          {text}
-          <meshStandardMaterial />
-          <Edges scale={1.0} threshold={15} color={mainColor} />
-        </Text3D>
-      </Center>
+    <Float enabled={true}>
+      <Text3D
+        font={'font/bigblue.json'}
+        size={6}
+        height={10}
+        position={[x, y, z]}
+        rotation={[rotationX, rotationY, rotationZ]}
+      >
+        {text}
+        <meshStandardMaterial />
+        <Edges scale={1.0} threshold={15} color={mainColor} />
+      </Text3D>
     </Float>
   )
 }
