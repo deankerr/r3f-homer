bc27ecdff58f729554810846069cbf55d8645fbf
Dean Kerr <dean.kerr@gmail.com>
2023-07-20 04:57:46 +1000

    Add r3f-perf hash

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 import { StatsGl } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Leva } from 'leva'
+import { Perf } from 'r3f-perf'
 import { Link, Outlet, useLocation } from 'react-router-dom'
 
 import { LoadingScene } from './scenes/LoadingScene'
@@ -16,6 +17,7 @@ export const Layout = () => {
   const location = useLocation()
   const hashLeva = location.hash.includes('leva')
   const hashStats = location.hash.includes('stats')
+  const hashPerf = location.hash.includes('perf')
 
   return (
     <div className="h-screen" onClick={handleInteraction}>
@@ -25,17 +27,22 @@ export const Layout = () => {
         <color attach="background" args={['#000']} />
         <LoadingScene />
         <Outlet />
-        {(hashStats || __DEV__) && <StatsGl horizontal={false} />}
+
+        {hashPerf ? (
+          <Perf position="bottom-left" antialias={false} logsPerSecond={2} chart={{ hz: 1, length: 30 }} />
+        ) : (
+          (hashStats || __DEV__) && <StatsGl horizontal={false} />
+        )}
       </Canvas>
 
       <Leva collapsed={!hashLeva} hidden={__PROD__ && !hashLeva} />
     </div>
   )
 }
-//<Perf position="bottom-left" antialias={false} logsPerSecond={2} chart={{ hz: 1, length: 30 }}
+
 const SceneNavigation = () => {
   return (
-    <div className="fixed bottom-0 z-10 flex w-screen  justify-center gap-2 sm:bottom-auto sm:top-0">
+    <div className="fixed bottom-0 z-10 flex w-screen justify-center gap-2 sm:bottom-auto sm:top-0">
       <SceneLink to="Rehetep" />
       <SceneLink to="Homer" />
       <SceneLink to="Believe" devOnly />
@@ -45,13 +52,15 @@ const SceneNavigation = () => {
 }
 
 const SceneLink = ({ to, devOnly = false }: { to: string; devOnly?: boolean }) => {
+  const location = useLocation()
+
   if (devOnly && !__DEV__) return null
 
   const text = devOnly ? 'text-blue-400' : 'text-fuchsia-400'
   const border = devOnly ? 'border-blue-400' : 'border-fuchsia-400'
 
   return (
-    <Link to={'/' + to.toLowerCase()}>
+    <Link to={'/' + to.toLowerCase() + location.hash}>
       <div className={`inline rounded-full border-2 bg-black bg-opacity-40 px-2 uppercase ${text} ${border}`}>{to}</div>
     </Link>
   )
